721da0f2cc1e30d35a3c8f6e5503c7c7147a7894
lburgazzoli <lburgazzoli@gmail.com>
2014-02-10 19:21:04 +0100

    default-tcp-example

--- sbox-hft/src/main/java/com/github/lburgazzoli/sandbox/hft/chronicle/tcp/netty/NettyChronicleSink.java
+++ sbox-hft/src/main/java/com/github/lburgazzoli/sandbox/hft/chronicle/tcp/netty/NettyChronicleSink.java
@@ -16,18 +16,38 @@
 package com.github.lburgazzoli.sandbox.hft.chronicle.tcp.netty;
 
 
+import io.netty.bootstrap.Bootstrap;
+import io.netty.buffer.PooledByteBufAllocator;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioSocketChannel;
 import net.openhft.chronicle.Chronicle;
 import net.openhft.chronicle.Excerpt;
 import net.openhft.chronicle.ExcerptAppender;
+import net.openhft.chronicle.ExcerptCommon;
 import net.openhft.chronicle.ExcerptTailer;
+import net.openhft.chronicle.tools.WrappedExcerpt;
 import org.jetbrains.annotations.NotNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.util.concurrent.atomic.AtomicBoolean;
 
+/**
+ *
+ */
 public class NettyChronicleSink implements Chronicle {
+    private static final Logger LOGGER = LoggerFactory.getLogger(NettyChronicleSink.class);
+
     private final Chronicle m_chronicle;
     private final String m_host;
     private final int m_port;
+    private final AtomicBoolean m_connected;
+    private Bootstrap m_bootstrap;
 
     /**
      * c-tor
@@ -40,42 +60,125 @@ public class NettyChronicleSink implements Chronicle {
         m_chronicle = chronicle;
         m_host = host;
         m_port = port;
+        m_connected = new AtomicBoolean(false);
+        m_bootstrap = null;
     }
 
     @Override
     public String name() {
-        return null;
+        return m_chronicle.name();
     }
 
     @NotNull
     @Override
     public Excerpt createExcerpt() throws IOException {
-        return null;
+        return new SinkExcerpt(m_chronicle.createExcerpt());
     }
 
     @NotNull
     @Override
     public ExcerptTailer createTailer() throws IOException {
-        return null;
+        return new SinkExcerpt(m_chronicle.createTailer());
     }
 
     @NotNull
     @Override
     public ExcerptAppender createAppender() throws IOException {
-        return null;
+        throw new UnsupportedOperationException();
     }
 
     @Override
     public long lastWrittenIndex() {
-        return 0;
+        return m_chronicle.lastWrittenIndex();
     }
 
     @Override
     public long size() {
-        return 0;
+        return m_chronicle.size();
     }
 
     @Override
     public void close() throws IOException {
+    }
+
+    // *************************************************************************
+    //
+    // *************************************************************************
+
+    private void connect() {
+        if(m_bootstrap == null) {
+            m_bootstrap = new Bootstrap();
+            m_bootstrap.group(new NioEventLoopGroup());
+            m_bootstrap.channel(NioSocketChannel.class);
+            m_bootstrap.option(ChannelOption.SO_KEEPALIVE,true);
+            m_bootstrap.option(ChannelOption.TCP_NODELAY,true);
+
+            if(true) {
+                m_bootstrap.option(
+                    ChannelOption.ALLOCATOR,
+                    new PooledByteBufAllocator(true));
+            }
+
+
+            InetSocketAddress addr = new InetSocketAddress(m_host,m_port);
+            m_bootstrap.connect(addr).addListener(new ChannelFutureListener() {
+                public void operationComplete(ChannelFuture future) {
+                    if (future.isDone() && future.isSuccess()) {
+                        m_connected.set(true);
+                        LOGGER.debug("Connected : {}",future.channel());
+                    } else if (!future.isSuccess() && !future.isCancelled()) {
+                        LOGGER.warn("Error", future.cause());
+                        m_connected.set(false);
+                    }
+                }
+            });
+
+            /**
+            m_bootstrap.handler(new NettyChannelInitializer(this.settings));
+
+            doConnect();
+            */
+        } else {
+            throw new RuntimeException("Connection is alive");
+        }
+    }
+
+    private boolean readNext() {
+        return false;
+    }
+
+    // *************************************************************************
+    //
+    // *************************************************************************
+
+    /**
+     *
+     */
+    private class SinkExcerpt extends WrappedExcerpt {
+
+        /**
+         * c-tor
+         *
+         * @param excerpt
+         * @throws IOException
+         */
+        @SuppressWarnings("unchecked")
+        public SinkExcerpt(ExcerptCommon excerpt) throws IOException {
+            super(excerpt);
+        }
+
+        @Override
+        public boolean nextIndex() {
+            return super.nextIndex() || readNext() && super.nextIndex();
+        }
+
+        @Override
+        public boolean index(long index) throws IndexOutOfBoundsException {
+            if (super.index(index)) {
+                return true;
+            }
+
+            return index >= 0 && readNext() && super.index(index);
+        }
     }
 }
