b33de405904292cf82a42f527e62acbe8712d127
lburgazzoli <lburgazzoli@gmail.com>
2014-02-10 18:51:01 +0100

    default-tcp-example

--- sbox-hft/src/main/java/com/github/lburgazzoli/sandbox/hft/chronicle/tcp/ChronicleDefaultTcp.java
+++ sbox-hft/src/main/java/com/github/lburgazzoli/sandbox/hft/chronicle/tcp/ChronicleDefaultTcp.java
@@ -18,26 +18,30 @@ package com.github.lburgazzoli.sandbox.hft.chronicle.tcp;
 import net.openhft.chronicle.Chronicle;
 import net.openhft.chronicle.ExcerptAppender;
 import net.openhft.chronicle.ExcerptTailer;
-import net.openhft.chronicle.IndexedChronicle;
-import net.openhft.chronicle.tcp.InProcessChronicleSink;
-import net.openhft.chronicle.tcp.InProcessChronicleSource;
-import net.openhft.chronicle.tools.ChronicleTools;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  *
  */
-public class ChronicleDefaultTcpMain {
-    private static final Logger LOGGER      = LoggerFactory.getLogger(ChronicleDefaultTcpMain.class);
-    private static final String BASEPATH    = "./data/chronicle/tcp";
-    private static final String PATH_SOURCE = BASEPATH + "/default.source";
-    private static final String PATH_SINK   = BASEPATH + "/default.sink";
-    private static final int    UPDATES     = 1000000;
-    private static final int    PORT        = 12345;
-    private static final char   CODE_PX     = 'P';
+public class ChronicleDefaultTcp {
+    public static final String BASEPATH    = "./data/chronicle/tcp";
+    public static final int    UPDATES     = 1000000;
+    public static final int    PORT        = 12345;
+    public static final char   CODE_PX     = 'P';
+
+    // *************************************************************************
+    //
+    // *************************************************************************
+
+    /**
+     *
+     * @param id
+     * @return
+     */
+    public static String path(final String id) {
+        return BASEPATH + "/" +  id;
+    }
 
     // *************************************************************************
     //
@@ -46,7 +50,7 @@ public class ChronicleDefaultTcpMain {
     /**
      *
      */
-    private static class DataWriter {
+    public static final class DataWriter {
         private final ExcerptAppender m_excerpt;
 
         /**
@@ -85,7 +89,7 @@ public class ChronicleDefaultTcpMain {
     /**
      *
      */
-    private static class DataReader {
+    public static final class DataReader {
         private final ExcerptTailer m_excerpt;
         private final AtomicInteger m_count;
 
@@ -128,50 +132,6 @@ public class ChronicleDefaultTcpMain {
          */
         public int count() {
             return m_count.get();
-        }
-    }
-
-    // *************************************************************************
-    //
-    // *************************************************************************
-
-    public static void main(String[] args) {
-        try {
-            ChronicleTools.warmup();
-            ChronicleTools.deleteOnExit(PATH_SOURCE);
-            ChronicleTools.deleteOnExit(PATH_SINK);
-
-            final Chronicle  source = new InProcessChronicleSource(new IndexedChronicle(PATH_SOURCE),PORT);
-            final Chronicle  sink   = new InProcessChronicleSink(new IndexedChronicle(PATH_SINK),"localhost",PORT);
-
-            final DataWriter writer = new DataWriter(source);
-            final DataReader reader = new DataReader(sink);
-
-            long start = System.nanoTime();
-            for (int i = 1; i <= UPDATES; i++) {
-                writer.write(i,"symbol", 99.9, i, 100.1, i + 1);
-            }
-
-            long mid = System.nanoTime();
-            while (reader.count() < UPDATES) {
-                reader.read();
-            }
-
-            long end = System.nanoTime();
-
-            LOGGER.info(
-                String.format("Took an average of %.2f us to write %d and %.2f us to read %d",
-                    (mid - start) / UPDATES / 1e3,
-                    UPDATES,
-                    (end - mid) / UPDATES / 1e3,
-                    reader.count())
-            );
-
-            source.close();
-            sink.close();
-
-        } catch(Exception e) {
-            LOGGER.warn("Main Exception", e);
         }
     }
 }
